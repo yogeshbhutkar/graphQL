3a4018b5a85e77ccd5934d93f08d3a0b09b76a5f
yogeshbhutkar <yogesh.bhutkar@rtcamp.com>
2024-12-06 16:39:51 +0530

    initialize GraphQL server with schema, database, and Apollo setup

---
+++ index.js
@@ -0,0 +1,63 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema.js";
+import db from './_db.js';
+
+const resolvers = { 
+	Query: {
+		games() {
+			return db.games;
+		},
+		reviews() {
+			return db.reviews;
+		},
+		authors() {
+			return db.authors;
+		},
+		review( _, args ) {
+			return db.reviews.find( review => review.id === args.id );
+		},
+		game( _, args ) {
+			return db.games.find( game => game.id === args.id );
+		},
+		author( _, args ) {
+			return db.authors.find( author => author.id === args.id );
+		}
+	},
+	Game: {
+		reviews( parent ) {
+			return db.reviews.filter( review => review.game_id === parent.id );
+		}
+	},
+	Author: {
+		reviews( parent ) {
+			return db.reviews.filter( review => review.author_id === parent.id );
+		}
+	},
+	Review: {
+		game( parent ) {
+			return db.games.find( game => game.id === parent.game_id );
+		},
+		author( parent ) {
+			return db.authors.find( author => author.id === parent.author_id );
+		}
+	},
+	Mutation: {
+		deleteGame( _, args ) {
+			db.games = db.games.filter( game => game.id !== args.id );
+			return db.games;
+		}
+	}
+};
+
+// setup server
+const server = new ApolloServer( {
+	typeDefs,
+	resolvers
+} );
+
+const { url } = await startStandaloneServer( server, {
+	listen: { port: 4000 }
+} );
+
+console.log( `🚀 Server ready at ${ url }` );
